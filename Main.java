1487c495115409394a40a3fb36ad91d7813142ac
167713 <100697872+167713@users.noreply.github.com>
2022-06-07 12:44:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner st = new Scanner(System.in);
+      String pesel="";
+      System.out.println("Podaj numer PESEL");
+      pesel=st.nextLine();
+
+    if(pesel.length()!=11)
+      System.out.println("Numer jest za krótki!");
+    else
+      System.out.println("PESEL poprawny");
+      int numer = pesel.charAt(9);
+      if(numer %2 != 0)
+        System.out.println("Mezczyzna");
+      else
+        System.out.println("Kobieta");
+  }
+}
+
+
+}
